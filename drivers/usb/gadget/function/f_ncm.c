f45fb3346776e1cbf90aebab767189a400bc1c6d
Santhosh <santhosh.user.why.red@gmail.com>
2024-07-24 13:16:27 +0000

    Revert "usb: gadget: ncm: Handle decoding of multiple NTB's in unwrap call"
    This reverts commit 8478d736d1ea82ecb5cdbb2cc62be1043d9425ac.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- drivers/usb/gadget/function/f_ncm.c
+++ drivers/usb/gadget/function/f_ncm.c
@@ -1208,8 +1208,7 @@ static int ncm_unwrap_ntb(struct gether *port,
 			  struct sk_buff_head *list)
 {
 	struct f_ncm	*ncm = func_to_ncm(&port->func);
-	unsigned char	*ntb_ptr = skb->data;
-	__le16		*tmp;
+	__le16		*tmp = (void *) skb->data;
 	unsigned	index, index2;
 	int		ndp_index;
 	unsigned	dg_len, dg_len2;
@@ -1220,10 +1219,6 @@ static int ncm_unwrap_ntb(struct gether *port,
 	const struct ndp_parser_opts *opts = ncm->parser_opts;
 	unsigned	crc_len = ncm->is_crc ? sizeof(uint32_t) : 0;
 	int		dgram_counter;
-	int		to_process = skb->len;
-
-parse_ntb:
-	tmp = (__le16 *)ntb_ptr;
 
 	/* dwSignature */
 	if (get_unaligned_le32(tmp) != opts->nth_sign) {
@@ -1260,11 +1255,8 @@ parse_ntb:
 			goto err;
 		}
 
-		/*
-		 * walk through NDP
-		 * dwSignature
-		 */
-		tmp = (__le16 *)(ntb_ptr + ndp_index);
+		/* walk through NDP */
+		tmp = (void *)(skb->data + ndp_index);
 		if (get_unaligned_le32(tmp) != ncm->ndp_sign) {
 			INFO(port->func.config->cdev, "Wrong NDP SIGN\n");
 			goto err;
@@ -1307,11 +1299,11 @@ parse_ntb:
 			if (ncm->is_crc) {
 				uint32_t crc, crc2;
 
-				crc = get_unaligned_le32(ntb_ptr +
+				crc = get_unaligned_le32(skb->data +
 							 index + dg_len -
 							 crc_len);
 				crc2 = ~crc32_le(~0,
-						 ntb_ptr + index,
+						 skb->data + index,
 						 dg_len - crc_len);
 				if (crc != crc2) {
 					INFO(port->func.config->cdev,
@@ -1331,8 +1323,8 @@ parse_ntb:
 							 dg_len - crc_len);
 			if (skb2 == NULL)
 				goto err;
-			skb_put_data(skb2, ntb_ptr + index,
-				     dg_len - crc_len);
+			memcpy(skb_put(skb2, dg_len - crc_len),
+			       skb->data + index, dg_len - crc_len);
 
 			skb_queue_tail(list, skb2);
 
@@ -1345,17 +1337,10 @@ parse_ntb:
 		} while (ndp_len > 2 * (opts->dgram_item_len * 2));
 	} while (ndp_index);
 
+	dev_kfree_skb_any(skb);
+
 	VDBG(port->func.config->cdev,
 	     "Parsed NTB with %d frames\n", dgram_counter);
-
-	to_process -= block_len;
-	if (to_process != 0) {
-		ntb_ptr = (unsigned char *)(ntb_ptr + block_len);
-		goto parse_ntb;
-	}
-
-	dev_consume_skb_any(skb);
-
 	return 0;
 err:
 	skb_queue_purge(list);
